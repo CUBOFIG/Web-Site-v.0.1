32eda1dc60be7dee889fdfbbf76da9dccccfb929
Heriberto Figueroa Michel <hfigueroa@ucol.mx>
2020-11-17 19:07:45 -0600

    second

--- src/shared/ButtonShared/ButtonShared.jsx
+++ src/shared/ButtonShared/ButtonShared.jsx
@@ -10,7 +10,6 @@ const ButtonShared = ({ children, type, onClick, buttonStyle, buttonSize }) => {
   const checkButtonStyle = STYLES.includes(buttonStyle)
     ? buttonStyle
     : STYLES[0];
-
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
   return (
